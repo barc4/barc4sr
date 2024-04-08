edf8b39ddabf5f318d7f75e20c21f94f9b534abd
celestre <rafael.celestre@synchrotron-soleil.fr>
2024-04-08 11:06:44 +0200

    refactoring: barc4sr.xoppy moved
    the functions that existed in barc4sr.xoppy were move to
    barc4sr.undulator for a more coherent packaging.
    
     Changes to be committed:
    	modified:   barc4sr/materials.py
    	modified:   barc4sr/undulator.py
    	deleted:    barc4sr/xoppy.py

--- barc4sr/materials.py
+++ barc4sr/materials.py
@@ -133,171 +133,171 @@ def reflectivity_curve(material: str, density: float, theta: float, ei: float, e
 
 
 
-#***********************************************************************************
-# reflected and absorbed power
-#***********************************************************************************
+# #***********************************************************************************
+# # reflected and absorbed power
+# #***********************************************************************************
 
-def pow_ref_and_abs_from_spectrum(theta: float, matDict: Dict[str, Any], spectrumSRdict: dict) -> dict:
-    """
-    Calculate power reflection and absorption from a given spectrum for various materials.
+# def pow_ref_and_abs_from_spectrum(theta: float, matDict: Dict[str, Any], spectrumSRdict: dict) -> dict:
+#     """
+#     Calculate power reflection and absorption from a given spectrum for various materials.
 
-    Args:
-        theta (float): The angle of incidence in mrad.
-        matDict (Dict[str, Any]): A dictionary containing material information. It should have the following structure:
-            {
-                "energy": array_like,
-                "material_1": {
-                    "density": float,
-                    "flag": int,
-                    "reflectivity": array_like,
-                    "reflected_power": float,
-                    "absorbed_power": float
-                },
-                "material_2": {
-                    "density": float,
-                    "flag": int,
-                    "reflectivity": array_like,
-                    "reflected_power": float,
-                    "absorbed_power": float
-                },
-                ...
-            }
-        spectrumSRdict (dict): A dictionary containing the spectral power information. It should have the following structure:
-            {
-                "spectrum": {
-                    "energy": array_like,
-                    "spectral_power": array_like
-                }
-            }
-    Returns:
-        dict: A dictionary containing updated material information after calculating reflection and absorption powers.
-    """
+#     Args:
+#         theta (float): The angle of incidence in mrad.
+#         matDict (Dict[str, Any]): A dictionary containing material information. It should have the following structure:
+#             {
+#                 "energy": array_like,
+#                 "material_1": {
+#                     "density": float,
+#                     "flag": int,
+#                     "reflectivity": array_like,
+#                     "reflected_power": float,
+#                     "absorbed_power": float
+#                 },
+#                 "material_2": {
+#                     "density": float,
+#                     "flag": int,
+#                     "reflectivity": array_like,
+#                     "reflected_power": float,
+#                     "absorbed_power": float
+#                 },
+#                 ...
+#             }
+#         spectrumSRdict (dict): A dictionary containing the spectral power information. It should have the following structure:
+#             {
+#                 "spectrum": {
+#                     "energy": array_like,
+#                     "spectral_power": array_like
+#                 }
+#             }
+#     Returns:
+#         dict: A dictionary containing updated material information after calculating reflection and absorption powers.
+#     """
 
-    matDict["energy"] = spectrumSRdict["spectrum"]["energy"]
+#     matDict["energy"] = spectrumSRdict["spectrum"]["energy"]
 
-    k = 0
-    for key in matDict:
-        if key != "energy":
-            matDict[key]["angle"] = np.degrees(theta*1e-3)
-            matDict[key]["reflectivity"], ene = reflectivity_curve(key, matDict[key]["density"], 
-                                                                   theta, 0, 0, 1, spectrumSRdict["spectrum"]["energy"], mat_flag=matDict[key]["flag"])  
+#     k = 0
+#     for key in matDict:
+#         if key != "energy":
+#             matDict[key]["angle"] = np.degrees(theta*1e-3)
+#             matDict[key]["reflectivity"], ene = reflectivity_curve(key, matDict[key]["density"], 
+#                                                                    theta, 0, 0, 1, spectrumSRdict["spectrum"]["energy"], mat_flag=matDict[key]["flag"])  
 
-            matDict[key]["reflected_power"] = integrate.trapezoid(np.multiply(matDict[key]["reflectivity"], 
-                                                                              spectrumSRdict["spectrum"]["spectral_power"]),
-                                                                  spectrumSRdict["spectrum"]["energy"])
-            matDict[key]["absorbed_power"] = integrate.trapezoid(np.multiply(1-matDict[key]["reflectivity"], 
-                                                                             spectrumSRdict["spectrum"]["spectral_power"]),
-                                                                 spectrumSRdict["spectrum"]["energy"])
-            print(f"> {key}")
-            mirr_reflec = matDict[key]["reflected_power"] 
-            mirr_absorb = matDict[key]["absorbed_power"]
-            print(f">> Reflected power:{mirr_reflec:.2f} W")
-            print(f">> Absorbed power :{mirr_absorb:.2f} W")
-            k+=1
+#             matDict[key]["reflected_power"] = integrate.trapezoid(np.multiply(matDict[key]["reflectivity"], 
+#                                                                               spectrumSRdict["spectrum"]["spectral_power"]),
+#                                                                   spectrumSRdict["spectrum"]["energy"])
+#             matDict[key]["absorbed_power"] = integrate.trapezoid(np.multiply(1-matDict[key]["reflectivity"], 
+#                                                                              spectrumSRdict["spectrum"]["spectral_power"]),
+#                                                                  spectrumSRdict["spectrum"]["energy"])
+#             print(f"> {key}")
+#             mirr_reflec = matDict[key]["reflected_power"] 
+#             mirr_absorb = matDict[key]["absorbed_power"]
+#             print(f">> Reflected power:{mirr_reflec:.2f} W")
+#             print(f">> Absorbed power :{mirr_absorb:.2f} W")
+#             k+=1
 
-    return matDict
+#     return matDict
 
 
-def pow_ref_and_abs_from_undulator_radiation(theta: float, reflec_plane: str, matDict: Dict[str, Any], URdict: Dict[str, Any], PowDenSR: Dict[str, Any]=None) -> Dict[str, Any]:
-    """
-    Calculate the reflected and absorbed power from undulator radiation.
+# def pow_ref_and_abs_from_undulator_radiation(theta: float, reflec_plane: str, matDict: Dict[str, Any], URdict: Dict[str, Any], PowDenSR: Dict[str, Any]=None) -> Dict[str, Any]:
+#     """
+#     Calculate the reflected and absorbed power from undulator radiation.
 
-    Args:
-        theta (float): Angle of incidence in milliradians.
-        reflec_plane (str): Plane of reflection ("h" for horizontal, "v" for vertical).
-        matDict (Dict[str, Any]): A dictionary containing material information with the following structure:
-            {
-                "material_symbol": {
-                    "density": float,
-                    "flag": int,
-                },
-            }     
-        URdict (Dict[str, Any]): Dictionary containing undulator radiation data.
-        PowDenSR (Dict[str, Any], optional): Dictionary containing integrated power density over the same window as URdict.
+#     Args:
+#         theta (float): Angle of incidence in milliradians.
+#         reflec_plane (str): Plane of reflection ("h" for horizontal, "v" for vertical).
+#         matDict (Dict[str, Any]): A dictionary containing material information with the following structure:
+#             {
+#                 "material_symbol": {
+#                     "density": float,
+#                     "flag": int,
+#                 },
+#             }     
+#         URdict (Dict[str, Any]): Dictionary containing undulator radiation data.
+#         PowDenSR (Dict[str, Any], optional): Dictionary containing integrated power density over the same window as URdict.
 
-    Returns:
-        Dict[str, Any]: Dictionary containing the calculated power values.
-            - "axis": Dictionary containing x and y axis values.
-                - "x": X-axis values.
-                - "y": Y-axis values.
-            - "projected_power": Dictionary containing projected power values.
-                - "map": Projected power density map.
-                - "CumPow": Cummulated projected power.
-                - "PowDenSRmax": Maximum projected power density.
-            - "reflected_power": Dictionary containing reflected power values.
-                - "map": Reflected power density map.
-                - "CumPow": Cummulated reflected power.
-                - "PowDenSRmax": Maximum reflected power density.
-            - "absorbed_power": Dictionary containing absorbed power values.
-                - "map": Absorbed power density map.
-                - "CumPow": Cummulated absorbed power.
-                - "PowDenSRmax": Maximum absorbed power density.
-    """
+#     Returns:
+#         Dict[str, Any]: Dictionary containing the calculated power values.
+#             - "axis": Dictionary containing x and y axis values.
+#                 - "x": X-axis values.
+#                 - "y": Y-axis values.
+#             - "projected_power": Dictionary containing projected power values.
+#                 - "map": Projected power density map.
+#                 - "CumPow": Cummulated projected power.
+#                 - "PowDenSRmax": Maximum projected power density.
+#             - "reflected_power": Dictionary containing reflected power values.
+#                 - "map": Reflected power density map.
+#                 - "CumPow": Cummulated reflected power.
+#                 - "PowDenSRmax": Maximum reflected power density.
+#             - "absorbed_power": Dictionary containing absorbed power values.
+#                 - "map": Absorbed power density map.
+#                 - "CumPow": Cummulated absorbed power.
+#                 - "PowDenSRmax": Maximum absorbed power density.
+#     """
     
-    powerDict = {
-        "axis": {
-            "x": None,
-            "y": None,
-        },    
-        "projected_power": {
-            "map": None,
-            "CumPow": None,
-            "PowDenSRmax": None
-        },
-        "reflected_power": {
-            "map": None,
-            "CumPow": None,
-            "PowDenSRmax": None
-        },
-        "absorbed_power": {
-            "map": None,
-            "CumPow": None,
-            "PowDenSRmax": None
-        },        
-    }
-    if reflec_plane == "h":
-        powerDict["axis"]["x"] = URdict["axis"]["x"]/np.sin(theta/1000)
-        powerDict["axis"]["y"] = URdict["axis"]["y"]
-    else:
-        powerDict["axis"]["x"] = URdict["axis"]["x"]
-        powerDict["axis"]["y"] = URdict["axis"]["y"]/np.sin(theta/1000)
+#     powerDict = {
+#         "axis": {
+#             "x": None,
+#             "y": None,
+#         },    
+#         "projected_power": {
+#             "map": None,
+#             "CumPow": None,
+#             "PowDenSRmax": None
+#         },
+#         "reflected_power": {
+#             "map": None,
+#             "CumPow": None,
+#             "PowDenSRmax": None
+#         },
+#         "absorbed_power": {
+#             "map": None,
+#             "CumPow": None,
+#             "PowDenSRmax": None
+#         },        
+#     }
+#     if reflec_plane == "h":
+#         powerDict["axis"]["x"] = URdict["axis"]["x"]/np.sin(theta/1000)
+#         powerDict["axis"]["y"] = URdict["axis"]["y"]
+#     else:
+#         powerDict["axis"]["x"] = URdict["axis"]["x"]
+#         powerDict["axis"]["y"] = URdict["axis"]["y"]/np.sin(theta/1000)
 
-    dx = powerDict["axis"]["x"][1]-powerDict["axis"]["x"][0]
-    dy = powerDict["axis"]["y"][1]-powerDict["axis"]["y"][0]
+#     dx = powerDict["axis"]["x"][1]-powerDict["axis"]["x"][0]
+#     dy = powerDict["axis"]["y"][1]-powerDict["axis"]["y"][0]
 
-    for key in matDict:
-        reflectivity, ene = reflectivity_curve(key, matDict[key]["density"], theta, 0, 0, 1, 
-                                               URdict["spectrum"]["energy"], mat_flag=matDict[key]["flag"])  
+#     for key in matDict:
+#         reflectivity, ene = reflectivity_curve(key, matDict[key]["density"], theta, 0, 0, 1, 
+#                                                URdict["spectrum"]["energy"], mat_flag=matDict[key]["flag"])  
 
-    powerDict["reflected_power"]["map"] = integrate.trapezoid(
-        np.multiply(reflectivity[:, np.newaxis, np.newaxis], URdict["spectral_power_3D"]), 
-        URdict["spectrum"]["energy"], axis=0
-        )*np.sin(theta/1000)
-    powerDict["reflected_power"]["CumPow"] = powerDict["reflected_power"]["map"].sum()*dx*dy
-    powerDict["reflected_power"]["PowDenSRmax"] = powerDict["reflected_power"]["map"].max()
+#     powerDict["reflected_power"]["map"] = integrate.trapezoid(
+#         np.multiply(reflectivity[:, np.newaxis, np.newaxis], URdict["spectral_power_3D"]), 
+#         URdict["spectrum"]["energy"], axis=0
+#         )*np.sin(theta/1000)
+#     powerDict["reflected_power"]["CumPow"] = powerDict["reflected_power"]["map"].sum()*dx*dy
+#     powerDict["reflected_power"]["PowDenSRmax"] = powerDict["reflected_power"]["map"].max()
 
-    if PowDenSR is None:
-        powerDict["projected_power"]["map"] = URdict["power_density"]["map"]*np.sin(theta/1000)
+#     if PowDenSR is None:
+#         powerDict["projected_power"]["map"] = URdict["power_density"]["map"]*np.sin(theta/1000)
 
-        powerDict["absorbed_power"]["map"] = integrate.trapezoid(
-            np.multiply(1-reflectivity[:, np.newaxis, np.newaxis], URdict["spectral_power_3D"]), 
-            URdict["spectrum"]["energy"], axis=0
-            )*np.sin(theta/1000)
-    else:
-        projPowDenSR = trim_and_resample_power_density(PowDenSR,
-                                                       dx=URdict["axis"]["x"][-1]-URdict["axis"]["x"][0], 
-                                                       dy=URdict["axis"]["y"][-1]-URdict["axis"]["y"][0],
-                                                       X=URdict["axis"]["x"],
-                                                       Y=URdict["axis"]["y"])
-        powerDict["projected_power"]["map"] = projPowDenSR["power_density"]["map"]*np.sin(theta/1000)
-        powerDict["absorbed_power"]["map"] = projPowDenSR["power_density"]["map"]*np.sin(theta/1000) - powerDict["reflected_power"]["map"]
-    powerDict["absorbed_power"]["CumPow"] = powerDict["absorbed_power"]["map"].sum()*dx*dy
-    powerDict["absorbed_power"]["PowDenSRmax"] = powerDict["absorbed_power"]["map"].max()
+#         powerDict["absorbed_power"]["map"] = integrate.trapezoid(
+#             np.multiply(1-reflectivity[:, np.newaxis, np.newaxis], URdict["spectral_power_3D"]), 
+#             URdict["spectrum"]["energy"], axis=0
+#             )*np.sin(theta/1000)
+#     else:
+#         projPowDenSR = trim_and_resample_power_density(PowDenSR,
+#                                                        dx=URdict["axis"]["x"][-1]-URdict["axis"]["x"][0], 
+#                                                        dy=URdict["axis"]["y"][-1]-URdict["axis"]["y"][0],
+#                                                        X=URdict["axis"]["x"],
+#                                                        Y=URdict["axis"]["y"])
+#         powerDict["projected_power"]["map"] = projPowDenSR["power_density"]["map"]*np.sin(theta/1000)
+#         powerDict["absorbed_power"]["map"] = projPowDenSR["power_density"]["map"]*np.sin(theta/1000) - powerDict["reflected_power"]["map"]
+#     powerDict["absorbed_power"]["CumPow"] = powerDict["absorbed_power"]["map"].sum()*dx*dy
+#     powerDict["absorbed_power"]["PowDenSRmax"] = powerDict["absorbed_power"]["map"].max()
 
-    powerDict["projected_power"]["CumPow"] = powerDict["projected_power"]["map"].sum()*dx*dy
-    powerDict["projected_power"]["PowDenSRmax"] = powerDict["projected_power"]["map"].max()
+#     powerDict["projected_power"]["CumPow"] = powerDict["projected_power"]["map"].sum()*dx*dy
+#     powerDict["projected_power"]["PowDenSRmax"] = powerDict["projected_power"]["map"].max()
 
-    return powerDict
+#     return powerDict
 
 
 #***********************************************************************************
