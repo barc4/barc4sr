7bf204614560ad272a97dc065c3ad348c0ae05b0
celestre <rafael.celestre@synchrotron-soleil.fr>
2024-06-04 06:32:31 +0200

    WIP: changes to reflectivity map calculations
     Changes to be committed:
    	modified:   barc4sr/materials.py

--- barc4sr/materials.py
+++ barc4sr/materials.py
@@ -8,9 +8,9 @@ __contact__ = 'rafael.celestre@synchrotron-soleil.fr'
 __license__ = 'GPL-3.0'
 __copyright__ = 'Synchrotron SOLEIL, Saint Aubin, France'
 __created__ = '26/JAN/2024'
-__changed__ = '15/MARCH/2024'
+__changed__ = '21/MAY/2024'
 
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 
 import numpy as np
 import xraylib
@@ -20,45 +20,116 @@ from xoppylib.scattering_functions.xoppy_calc_f1f2 import xoppy_calc_f1f2
 # reflectivity curves
 #***********************************************************************************
 
-def reflectivity_map(material: str, density: float, thetai: float, thetaf: float,
-                     ntheta: int, ei: float, ef: float, ne: int,
-                     e_axis: Optional[np.ndarray] = None, mat_flag: int = 0) -> Tuple[np.ndarray, np.ndarray]:
+def nist_compound_list() -> list:
     """
-    Compute a reflectivity map for a given material over a range of angles and energies.
+    Retrieve and returns the list of NIST compounds.
+
+    Note:
+        This function is taken from the xoppylib interface for xraylib.
+    """
+    return xraylib.GetCompoundDataNISTList()
+
+
+def density_element(material:Union[str, int, float], verbose: bool = False) -> float:
+    """
+    Get the density of an element based on its atomic number or symbol.
 
     Args:
-        material (str): The material's name.
-        density (float): The material's density in g/cm^3.
-        thetai (float): The initial angle of incidence in milliradians (mrad).
-        thetaf (float): The final angle of incidence in milliradians (mrad).
-        ntheta (int): The number of angles between thetai and thetaf.
-        ei (float): The initial energy in electron volts (eV).
-        ef (float): The final energy in electron volts (eV).
-        ne (int): The number of energy points between ei and ef.
-        e_axis (Optional[np.ndarray], optional): An array representing the energy axis. Defaults to None.
-        mat_flag (int, optional): A flag indicating special treatment for the material. Defaults to 0.
+        material (str | int | float): The atomic symbol (str), atomic number (int), or atomic number (float).
+        verbose (bool): If True, prints the density. Defaults to False.
 
     Returns:
-        Tuple[np.ndarray, np.ndarray]: A tuple containing two NumPy arrays:
-            - The reflectivity map with shape (ntheta, ne) if e_axis is None, else (ntheta, len(e_axis)).
-            - The energy axis.
+        float: The density of the element.
+
+    Note:
+        This function is taken from the xoppylib interface for xraylib.
+    """
+    if isinstance(material, str):
+        Z = xraylib.SymbolToAtomicNumber(material)
+    elif isinstance(material, int):
+        Z = material
+    elif isinstance(input, float):
+        Z = int(material)
+    else:
+        raise Exception("Bad input.")
+    density = xraylib.ElementDensity(Z)
+    if verbose:
+        print("Density for %s (Z=%d): %6.3f g/cm3"%(material, Z, density))
+    return density
+
+
+def density_nist(material: Union[str, int], verbose: bool = False) -> float:
+    """
+    Get the density of a NIST compound based on its name or index.
+
+    Args:
+        material (str | int): The compound name (str) or index (int).
+        verbose (bool): If True, prints the density. Defaults to False.
+
+    Returns:
+        float: The density of the compound.
+
+    Note:
+        This function is taken from the xoppylib interface for xraylib.
     """
 
-    theta = np.linspace(thetai, thetaf, ntheta)
+    if isinstance(material, str):
+        Zarray = xraylib.GetCompoundDataNISTByName(material)
+        density = Zarray["density"]
+        if verbose:
+            print("Density for %s: %6.3f g/cm3"%(material, density))
+    elif isinstance(material, int):
+        Zarray = xraylib.GetCompoundDataNISTByIndex(material)
+        density = Zarray["density"]
+        if verbose:
+            print("Density for NIST compound with index %d: %6.3f " % (material, density))
 
-    if e_axis is None:
-        reflectivityMap = np.zeros((ntheta, ne))
+    return density
+
+
+def get_density(material: str, verbose: bool = False) -> float:
+    """
+    Determine the density of a material based on its descriptor.
+
+    Args:
+        material (str): The descriptor of the material (atomic symbol, compound name, etc.).
+        verbose (bool): If True, prints the density. Defaults to False.
+
+    Returns:
+        float: The density of the material.
+
+    Note:
+        This function is taken from the xoppylib interface for xraylib.
+    """
+        
+    if not isinstance(material, str):
+        raise Exception("descriptor must be a string!")
+    kind = -1
+    if len(material) <=2:
+        Z = xraylib.SymbolToAtomicNumber(material)
+        if Z > 0:
+            kind = 0
+    elif material in nist_compound_list():
+        kind = 2
     else:
-        reflectivityMap = np.zeros((ntheta, len(e_axis)))
+        try:
+            xraylib.CompoundParser(material)
+            kind = 1
+        except:
+            pass
 
-    for k, th in enumerate(theta):
-        reflectivityMap[k,:], ene = reflectivity_curve(material, density, th, ei, ef, ne, e_axis, mat_flag)
-
-    return reflectivityMap, ene
-
+    if kind == 0:
+        return density_element(material, verbose)
+    elif kind == 1:
+        raise Exception("cannot retrieve density for a compound (%s): it must be defined by user" % material)
+    elif kind == 2:
+        return density_nist(material, verbose)
+    else:
+        raise Exception("Unknown descriptor: %s" % material)
+        
 
 def reflectivity_curve(material: str, density: float, theta: float, ei: float, ef: float, ne: int,
-                       e_axis: Optional[np.ndarray] = None, mat_flag: int = 0) -> Tuple[np.ndarray, np.ndarray]:
+                       e_axis: Optional[np.ndarray] = None) -> dict:
     """ 
     Calculate the reflectivity for a given material and conditions.
 
@@ -70,13 +141,22 @@ def reflectivity_curve(material: str, density: float, theta: float, ei: float, e
         ef (float): The final energy in electron volts (eV).
         ne (int): The number of energy steps.
         e_axis (Optional[np.ndarray], optional): An array representing the energy axis for point-wise calculation. Defaults to None.
-        mat_flag (int, optional): A parameter to control material parsing. Defaults to 0.
 
     Returns:
-        Tuple[np.ndarray, np.ndarray]: A tuple containing two NumPy arrays:
-            - The reflectivity values.
-            - The corresponding energy values.
+        dict: A dictionary containing:
+            - "reflectivity" (np.ndarray): The reflectivity values.
+            - "energy" (np.ndarray): The corresponding energy values.
+            - "theta" (float): The angle of incidence.
+            - "material" (str): The material's name. 
     """
+
+    if density == -1:
+        density = get_density(material, True)
+
+    if len(material) <=2:
+        mat_flag = 0
+    else:
+        mat_flag = 1
 
     if e_axis is None:
         out_dict =  xoppy_calc_f1f2(
@@ -101,41 +181,105 @@ def reflectivity_curve(material: str, density: float, theta: float, ei: float, e
         energy_axis = out_dict["data"][0,:]
         reflectivity = out_dict["data"][-1,:]
     else:
-        k = 0
+        energy_axis = np.array([])
+        reflectivity = np.array([])
         for E in e_axis:
-            out_dict =  xoppy_calc_f1f2(
-            descriptor   = material,
-            density      = density,
-            MAT_FLAG     = mat_flag,
-            CALCULATE    = 9,
-            GRID         = 2,
-            GRIDSTART    = E,
-            GRIDEND      = E,
-            GRIDN        = 1,
-            THETAGRID    = 0,
-            ROUGH        = 0.0,
-            THETA1       = theta,
-            THETA2       = 5.0,
-            THETAN       = 50,
-            DUMP_TO_FILE = 0,
-            FILE_NAME    = "%s.dat"%material,
-            material_constants_library = xraylib,
+            out_dict = xoppy_calc_f1f2(
+                descriptor=material,
+                density=density,
+                MAT_FLAG=mat_flag,
+                CALCULATE=9,
+                GRID=2,
+                GRIDSTART=E,
+                GRIDEND=E,
+                GRIDN=1,
+                THETAGRID=0,
+                ROUGH=0.0,
+                THETA1=theta,
+                THETA2=5.0,
+                THETAN=50,
+                DUMP_TO_FILE=0,
+                FILE_NAME="%s.dat" % material,
+                material_constants_library=xraylib,
             )
-            if k == 0:
-                energy_axis = np.asarray(out_dict["data"][0,:], dtype="float64")
-                reflectivity = np.asarray(out_dict["data"][-1,:], dtype="float64")
-                k+=1
-            else:
-                energy_axis = np.concatenate((energy_axis, np.asarray(out_dict["data"][0,:], dtype="float64")), axis=0)
-                reflectivity  = np.concatenate((reflectivity, np.asarray(out_dict["data"][-1,:], dtype="float64")), axis=0)
-            
-    return reflectivity, energy_axis
+            energy_axis = np.append(energy_axis, out_dict["data"][0, :])
+            reflectivity = np.append(reflectivity, out_dict["data"][-1, :])
+
+    return {
+        "reflectivity": reflectivity,
+        "energy": energy_axis,
+        "theta": theta,
+        "material": material,
+    }
 
 
+def reflectivity_map(material: str, density: float, thetai: float, thetaf: float,
+                     ntheta: int, ei: float, ef: float, ne: int,
+                     e_axis: Optional[np.ndarray] = None) -> dict:
+    """
+    Compute a reflectivity map for a given material over a range of angles and energies.
 
-# #***********************************************************************************
-# # reflected and absorbed power
-# #***********************************************************************************
+    Args:
+        material (str): The material's name.
+        density (float): The material's density in g/cm^3.
+        thetai (float): The initial angle of incidence in milliradians (mrad).
+        thetaf (float): The final angle of incidence in milliradians (mrad).
+        ntheta (int): The number of angles between thetai and thetaf.
+        ei (float): The initial energy in electron volts (eV).
+        ef (float): The final energy in electron volts (eV).
+        ne (int): The number of energy points between ei and ef.
+        e_axis (Optional[np.ndarray], optional): An array representing the energy axis. Defaults to None.
+
+    Returns:
+        dict: A dictionary containing:
+            - "reflectivity_map" (np.ndarray): The reflectivity map.
+            - "energy" (np.ndarray): The energy axis.
+            - "theta" (np.ndarray): The angle axis.
+            - "material" (str): The material's name. 
+    """
+
+    if density == -1:
+        density = get_density(material)
+        
+    theta = np.linspace(thetai, thetaf, ntheta)
+
+    if e_axis is None:
+        reflectivity_map = np.zeros((ntheta, ne))
+    else:
+        reflectivity_map = np.zeros((ntheta, len(e_axis)))
+
+    for k, th in enumerate(theta):
+        result = reflectivity_curve(material, density, th, ei, ef, ne, e_axis)
+        reflectivity_map[k, :] = result["reflectivity"]
+    return {
+        "reflectivity": reflectivity_map,
+        "energy": result["energy"],
+        "theta": theta,
+        "material": material,
+    }
+
+
+def ml_reflectivity_curve():
+    #TODO: interface for xoppy and DarpanX
+    pass
+
+
+def ml_reflectivity_map():
+    pass
+
+
+def crystal_reflectivity_curve():
+    #TODO:  interface for xoppy and CrystalPy
+    pass
+
+
+def crystal__reflectivity_map():
+    pass
+
+
+#***********************************************************************************
+# reflected and absorbed power
+#***********************************************************************************
 
 # def pow_ref_and_abs_from_spectrum(theta: float, matDict: Dict[str, Any], spectrumSRdict: dict) -> dict:
 #     """
@@ -301,5 +445,5 @@ def reflectivity_curve(material: str, density: float, theta: float, ei: float, e
 
 
 #***********************************************************************************
-# transmission curves
+# transmitted and absorbed power
 #***********************************************************************************
